ddd48e2d67f2ee67a87e1636234673cb8dc3409d
Callista <westcallista@gmail.com>
2022-02-07 15:09:52 -0800

    second activity with working mod button

--- Proj407/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ Proj407/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,12 +3,39 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private EditText edittext1, edittext2;
+    private Button buttonMod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        modListenerOnButton();
+    }
+
+    public void modListenerOnButton() {
+        edittext1 = (EditText) findViewById(R.id.editText1);
+        edittext2 = (EditText) findViewById(R.id.editText2);
+        buttonMod = (Button) findViewById(R.id.button);
+
+        buttonMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String value1=edittext1.getText().toString();
+                String value2=edittext2.getText().toString();
+                int a=Integer.parseInt(value1);
+                int b=Integer.parseInt(value2);
+                int sum=a % b;
+                Toast.makeText(getApplicationContext(),String.valueOf(sum), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
